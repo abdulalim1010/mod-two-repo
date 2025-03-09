52c3de2ff327236e47a6a57e869f3bff6e1f08e7
abdulalilm1010 <abdulalim243457@gmail.com>
2025-03-09 21:32:22 +0600

    hard item

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const person = {
-  name: "Hena", age: 25,
+  name: 'Hena', age: 25,
   friends:["alin", "sara", "jim"]
 };
 const newPerson=JSON.stringify(person);
-console.log(newPerson);
+const newperson2 = JSON.parse(newPerson);
